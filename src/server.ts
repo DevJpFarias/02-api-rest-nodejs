c29fbd108d2e00d4f7e33a1cea789d439122d059
João <joaopfc2764@gmail.com>
2023-04-30 14:44:26 -0300

    create queries on project

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,31 @@
 import Fastify from 'fastify'
+import crypto from 'node:crypto'
 import { knex } from './database'
 
 const fastify = Fastify()
 
-fastify.get('/hello', async function (request, reply) {
+fastify.get('/tables', async function (request, reply) {
   const tables = await knex('sqlite_schema').select('*')
 
   return tables
+})
+
+fastify.post('/transaction', async function (request, reply) {
+  const transaction = await knex('transactions')
+    .insert({
+      id: crypto.randomUUID(),
+      title: 'Transação de teste',
+      amount: 1000,
+    }).returning('*')
+
+  return transaction
+})
+
+fastify.get('/transactions', async function (request, reply) {
+  const transactions = await knex('transactions')
+    .select('*')
+
+  return transactions
 })
 
 fastify.listen({
