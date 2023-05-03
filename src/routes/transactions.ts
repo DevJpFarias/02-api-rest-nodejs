25317f733d5017fb01701d98dc4bab5f1ec80192
João <joaopfc2764@gmail.com>
2023-05-03 11:59:48 -0300

    create list transactions route

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -21,4 +21,12 @@ export async function transactionsRoutes(app: FastifyInstance) {
 
     return reply.status(201).send()
   })
+
+  app.get('/', async () => {
+    const transactions = await knex('transactions').select()
+
+    return {
+      transactions
+    }
+  })
 }
