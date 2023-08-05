43468accb6b4c743d8f66031770c2433666216cb
João <joaopfc2764@gmail.com>
2023-08-04 21:31:12 -0300

    refactor: accept tests env to use new database

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,15 +1,21 @@
-import 'dotenv/config'
+import { config } from 'dotenv'
 import { z } from 'zod'
+
+if (process.env.NODE_ENV === 'test') {
+  config({ path: '.env.test' })
+} else {
+  config()
+}
 
 const envSchema = z.object({
   DATABASE_URL: z.string(),
   SERVER_PORT: z.string(),
-  NODE_ENV: z.enum(['development', 'test', 'production']).default('production')
+  NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
 })
 
 const _env = envSchema.safeParse(process.env)
 
-if(_env.success === false) {
+if (_env.success === false) {
   console.log('Invalid environment variables!', _env.error.format())
 
   throw new Error('Invalid environment variables.')
